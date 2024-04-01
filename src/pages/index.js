5e0e15dca9db5195263959f5cd5662efa5e0a5f3
Hassan Ali <hassan4np@gmail.com>
2024-04-01 11:31:44 +0600

    update  code

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { Inter } from "next/font/google";
 import ClientLayout from "@/components/Layout/ClientLayout";
 import HeroSlider from "@/components/pages/Home/HeroSlider";
 import Achievement from "@/components/pages/Home/Achievement";
-import bestsell from "@/components/pages/Home/bestsell";
-import business from "@/components/pages/Home/business";
-import packs_banner from "@/components/pages/Home/packs_banner";
+import Bestsell from "@/components/pages/Home/bestsell";
+import Business from "@/components/pages/Home/business";
+import Packs_banner from "@/components/pages/Home/packs_banner";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,9 +15,9 @@ export default function Home() {
     <ClientLayout>
       <HeroSlider />
       <Achievement />
-      <bestsell/>
-      <business/>
-      <packs_banner/>
+      <Bestsell/>
+      <Business/>
+      <Packs_banner/>
     </ClientLayout>
   );
 }
