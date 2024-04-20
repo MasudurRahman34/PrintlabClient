b571d8c6eaabe34b4a6e7a8d9bba52a19ea11603
Hassan Ali <hassan4np@gmail.com>
2024-04-20 12:50:45 +0600

    complate payment design

---
+++ src/pages/my-account/delivery-payment.js
@@ -0,0 +1,13 @@
+import ClientLayout from '@/components/Layout/ClientLayout';
+import Deliverypayment from '@/components/dashboard/Deliverypayment';
+import React from 'react';
+
+const Delivery = () => {
+    return (
+        <ClientLayout>
+            <Deliverypayment/>
+        </ClientLayout>
+    );
+};
+
+export default Delivery;
