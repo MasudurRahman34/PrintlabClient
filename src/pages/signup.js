417b622f41aeb00a899d8bb7173ee2a6a72edc62
Hassan Ali <hassan4np@gmail.com>
2024-04-23 13:01:58 +0600

    complate validation login and signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,26 +1,30 @@
 import ClientLayout from "@/components/Layout/ClientLayout";
 import Link from "next/link";
-const signup = () => {
+import { useForm } from "react-hook-form";
+const Signup = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <ClientLayout>
       <>
-   
         <div className="container mx-auto mb-2 md:mb-5 mt-10 px-2 ">
           <div className="flex justify-center ">
-            <div>
+            <div className="w-[50%]">
               <h4 className=" text-center font-semibold md:font-bold py-5px-5 mt-5 mb-5 text-lg md:text-xl lg:text-2xl xl:text-3xl ">
                 Register
               </h4>
-              <form>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <div className=" input-from w-full">
                   <div className="w-full mb-2 md:mb-5 ">
-                    <label className=" text-[12px] md:text-base text-typography font-medium">
+                    <label className=" text-[12px] md:text-base text-typography font-medium ">
                       Email
                     </label>
                     <input
-                      className="border text-typography text-[12px] md:text-[14px] px-2 py-2 w-full"
-                      placeholder="Enter Email"
                       type="email"
+                      placeholder="Enter Email"
+                      className="border text-typography text-[12px] md:text-[14px] px-2 py-2 w-full outline-none "
+                      {...register("email", { required: true })}
                     />
                   </div>
                   <div className="w-full mb-2 md:mb-5">
@@ -28,76 +32,88 @@ const signup = () => {
                       Password
                     </label>
                     <input
-                      className="border text-typography text-[12px] md:text-[14px] px-2 py-2 w-full"
-                      placeholder="Enter Password"
                       type="password"
+                      placeholder="Enter Password"
+                      className="border text-typography text-[12px] md:text-[14px] px-2 py-2 w-full outline-none"
+                      {...register("password", {
+                        required: true,
+                        minLength: 8,
+                      })}
                     />
                   </div>
                   <div className="w-full mb-2 md:mb-5">
-                  <label className=" text-[12px] block md:text-base text-typography font-medium">
-                  Customer Type
+                    <label className=" text-[12px] block md:text-base text-typography font-medium">
+                      Customer Type
                     </label>
-                    <select id="countries" class=" form-control border text-[12px] md:text-[14px] text-typography px-2 py-2 w-full">
-                      <option selected>Please select</option>
-                      <option value="US">Charity or Community Group</option>
-                      <option value="CA">Publin services of Edcation</option>
-                      <option value="FR">Event / Entertainment</option>
-                      <option value="DE">Personal</option>
-                      <option value="DE">Designer / Agency</option>
-                      <option value="DE">Small Business / Retail</option>
-                      <option value="DE">Comporate / office</option>
-                      <option value="DE">Printer / Print Reseller</option>
+                    {/* <select
+                      id="countries"
+                      class=" form-control border text-[12px] md:text-[14px] text-typography px-2 py-2 w-full"
+                    >  */}
+                    <select {...register("customerValue")} className=" form-control border text-[12px] md:text-[14px] text-typography px-2 py-2 w-full outline-none ">
+                      <option selected disabled > Please select</option>
+                      <option value="Charity or Community Group">Charity or Community Group</option>
+                      <option value="Publin services of Edcation">Publin services of Edcation</option>
+                      <option value="Event / Entertainment">Event / Entertainment</option>
+                      <option value="Personal">Personal</option>
+                      <option value="Designer / Agency">Designer / Agency</option>
+                      <option value="Small Business / Retail">Small Business / Retail</option>
+                      <option value="Comporate / office">Comporate / office</option>
+                      <option value="Printer / Print Reseller">Printer / Print Reseller</option>
                     </select>
                   </div>
                   <div className=" mb-4 ">
                     <div>
                       <input
                         type="checkbox"
-                        id="login"
-                        name="vehicle1"
+                        id="signup"
+                        name="signup"
                         value="checkout"
+                        required
                       />
                       <label
-                        for="vehicle1"
+                        for="signup"
                         className="text-[12px] md:text-base   text-secondgraphy"
                       >
                         {" "}
-                        Sign up for exclusive offers and discounts by email and <br/> also receive 15% off your first order
+                        Sign up for exclusive offers and discounts by email and{" "}
+                        <br /> also receive 15% off your first order
                       </label>
                     </div>
-                
                   </div>
                   <div className=" mb-4 ">
                     <div>
                       <input
                         type="checkbox"
-                        id="login"
-                        name="vehicle1"
+                        id="accept"
+                        name="accept"
                         value="checkout"
+                        for="accept"
+                        required
                       />
                       <label
-                        for="vehicle1"
+                        for="accept"
                         className="text-[12px] md:text-base   text-secondgraphy"
                       >
                         {" "}
-                        I have read, understood and accept the Privacy <span className="text-[#33A6DC] hover:underline ">policy.</span>
+                        I have read, understood and accept the Privacy{" "}
+                        <span className="text-[#33A6DC] hover:underline ">
+                          policy.
+                        </span>
                       </label>
                     </div>
                   </div>
-          
+
                   <div className="mb-2">
                     <button className=" text-base md:text-lg hover:bg-[#eed680] font-semibold md:font-bold text-white text-center py-2 w-full rounded-md  bg-primary">
                       Sign Up
                     </button>
                   </div>
                   <div className="w-full mb-5">
-                    <p className="text-[12px] md:text-[14px] font-normal text-center text-typography">Have an account?
-                {" "}
+                    <p className="text-[12px] md:text-[14px] font-normal text-center text-typography">
+                      Have an account?{" "}
                       <Link href="/login">
                         {" "}
-                        <span className="underline text-[#4DA0D7]">
-                          Login{" "}
-                        </span>
+                        <span className="underline text-[#4DA0D7]">Login </span>
                       </Link>
                     </p>
                   </div>
@@ -111,4 +127,4 @@ const signup = () => {
   );
 };
 
-export default signup;
+export default Signup;
